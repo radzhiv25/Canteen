82bedcfece2729fc87b98d42ce7def6e35306115
OneEyedDragon1102 <adityakhamare29@gmail.com>
2023-10-19 00:52:42 +0530

    feat. added login

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 mongoose.connect('mongodb://127.0.0.1/canteen');
 
 app.post('/register', async (req, res) => {
-    console.log("hey how you doin figs")
+    // console.log("hey how you doin figs")
     const { enrollment, name, email, password } = req.body;
         const newUser = new UserModel.User({
             enrollment,
@@ -20,9 +20,29 @@ app.post('/register', async (req, res) => {
         });
 
         await newUser.save();
-        console.log("User Created successfully")
+        // console.log("User Created successfully")
         res.json({ message: 'User registration successful' });
 });
+
+app.post('/login', async(req, res) => {
+    // console.log("Hey signed up user");
+    const{email, password} = req.body;
+    
+    const user = await UserModel.findUserByEmail(email);
+    // console.log(user)
+    if(user){
+        console.log(user.password, user.email, password)
+        if(user.password === password){
+            res.json("Success");
+        }
+        else{
+            res.json("Password and Email do not match");
+        }
+    }
+    else{
+        res.json("No user found");
+    }
+})
 
 app.listen(3002, () =>{
     console.log("server running");
