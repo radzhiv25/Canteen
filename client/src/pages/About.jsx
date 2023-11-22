508c02fe703d10cc018a6877f5148aa6e1bee347
radzhiv <radzhivkrishna@gmail.com>
2023-11-22 15:31:46 +0530

    Update: Minor changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,15 +2,18 @@
 
 export const About = () => {
   return (
-    <div className="h-screen text-gray-600 py-10">
-      <h1 className="text-center text-3xl font-semibold">About Us</h1>
-      <div className="md:w-1/2 mx-auto">
-        <p className="mt-10 text-center font-medium">
+    <div className="h-screen py-10 text-black">
+      <h1 className="font-bold bg-gradient-to-r from-purple-500 via-fuchsia-500 to-pink-400 bg-clip-text text-transparent text-3xl text-center">Our Story</h1>
+      <div className="md:w-1/2 md:mx-auto font-medium mx-5 text-justify space-y-5 py-5">
+        <p className="">
           Welcome to Canteen App, where convenience meets deliciousness!
           We are your go-to solution for a seamless and enjoyable dining
           experience. Whether you are a busy professional, a student on the go,
           or someone who simply loves great food, our app is designed with you
           in mind.
+        </p>
+        <p className="">
+          The idea was just an instant thought, as a student we used to face the problem of being stranded in the line for several minutes just to get food. So we thought of a solution to this problem and came up with this idea of an app that will help students to order food from their phone and get it delivered to them.
         </p>
       </div>
     </div>
--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,18 +1,88 @@
+/* eslint-disable react/jsx-key */
 // import React from 'react'
 
-import { FoodCard } from "../components/FoodCard"
+import { FoodCard } from "../components/FoodCard";
+// import LoginImg from "../assets/burgerImg.avif";
 
 export const Menu = () => {
+  const menuItems = [
+    {
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },
+    {
+      image: "../src/assets//MenuItems/chai.jpeg",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },
+    {
+      image: "../src/assets/MenuItems/chole-bhature.jpg",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },
+    {
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },{
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },{
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },{
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },{
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },{
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },{
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },{
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },{
+      image: "../src/assets/burgerImg.avif",
+      title: "Burger",
+      rating: "4.5",
+      price: "50",
+    },
+  ];
   return (
-    <div className='text-black h-screen'>
-      <div className="mt-16 grid grid-cols-4 w-2/3 mx-auto gap-5">
-        <FoodCard title='Burger' price='40' />
-        <FoodCard title='Burger' price='40' />
-        <FoodCard title='Burger' price='40' />
-        <FoodCard title='Burger' price='40' />
-        <FoodCard title='Burger' price='40' />
-        <FoodCard title='Burger' price='40' />
+    <div className="text-black h-full">
+      <div className="mt-16 grid md:grid-cols-4 md:w-2/3 md:mx-auto mx-10 gap-5">
+        {menuItems.map((item) => (
+          <FoodCard
+            image={item.image}
+            title={item.title}
+            rating={item.rating}
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
