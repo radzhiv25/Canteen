bdd32373cc3f6383a9d1aebc20532f43869f7a52
radzhiv <radzhivkrishna@gmail.com>
2023-11-22 15:55:11 +0530

    Update: Foodcard Updated'

--- client/src/components/FoodCard.jsx
+++ client/src/components/FoodCard.jsx
@@ -1,21 +1,35 @@
 /* eslint-disable react/prop-types */
 // import React from 'react'
 import { useState } from "react";
-import BurgerImg from '../assets/burgerImg.avif'
+// import BurgerImg from "../assets/burgerImg.avif";
+
 export const FoodCard = (props) => {
   const [count, setCount] = useState(false);
+
+  const addBtn = () => {
+    if(count < 5){
+      setCount(count + 1)
+    }
+  }
+  const removeBtn = () => {
+    if(count > 1){
+      setCount(count - 1)
+    }
+  }
   return (
     <div className="border p-2 rounded">
-      <h1 className="text-2xl font-semibold">{props.title}</h1>
-      <img src={BurgerImg} alt="" className="rounded"/>
-      <div className="py-2 flex justify-between items-center">
-      <p className="font-medium text-lg">Rs {props.price}</p>
-      <button
-        className="p-2 text-white bg-green-600 rounded"
-        onClick={() => setCount(count + 1)}
-      >
-        Add to cart <span>{count}</span>
-      </button>
+      <img src={props.image} alt="" className="rounded" />
+      <div className="my-2 flex justify-between items-center">
+        <h1 className="text-xl font-semibold">{props.title}</h1>
+        <span className="flex border rounded-full p-1">{props.rating} <p className="ml-1 border-l px-1">Rating</p></span>
+      </div>
+      <div className="py-2 flex justify-between items-center border-t mt-2">
+        <p className="font-medium text-lg">Rs {props.price}</p>
+        <div className="">
+          <button onClick={addBtn}>+</button>
+          <span>{count}</span>
+          <button onClick={removeBtn}>-</button>
+        </div>
       </div>
     </div>
   );
